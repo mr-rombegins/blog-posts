37be54fb7b9fd0ac28dc9605eccc6a0fee7561a8
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-04-26 14:17:00 +0000

    Blog content and Navbar

---
+++ pages/posts/[postId].jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const PostDetail = () => {
+  const router = useRouter();
+  const { postId } = router.query;
+
+  return <div>PostDetail - {postId}</div>;
+};
+
+export default PostDetail;
